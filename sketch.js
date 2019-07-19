c2a1454fdc6fd962dbbbbed316ad84472f8db7ba
ErickASC5 <ejemiranda@gmail.com>
2019-07-19 15:22:39 -0400

    Multiple enemies with moving and increased speed, with ship that moves with WASD, and commented out old code underneath for reference

--- sketch.js
+++ sketch.js
@@ -1,3 +1,61 @@
+let bounceplayerX = false;
+let shipShift = -1;
+let shipSpeed = 1;
+let shipPositionX = 30, shipPositionY = 15;
+let playerX = 0, playerY = 0, w = 0, h = 0;
+let ymove = 2, playerXmove = 2;
+function setup(){
+   createCanvas(800,800);
+   background(0);
+   playerX = (width / 2) - 30;
+   playerY = height - 100;
+}
+function draw(){
+   background(0);
+   if(shipPositionX <= 30){
+      bounceShipX = false;
+      shipShift += 1;
+   }
+   if(shipPositionX >= width - 670){
+      bounceShipX = true;
+      shipShift += 1;
+   }
+   if(shipShift == 3){
+       shipPositionY += 50;
+       shipShift = 0;
+       shipSpeed += 0.25;
+   }
+   for(let i = 0; i < 700; i += 100){
+       for(let j = 0; j < 300; j += 60){
+      rect(shipPositionX + i, shipPositionY + j, 50, 50);
+       }
+   }
+      if(bounceShipX == false){
+          shipPositionX += 1 * shipSpeed;
+      }
+      else if(bounceShipX == true){
+          shipPositionX -= 1 * shipSpeed;
+      }
+      if(keyIsDown(83)){
+       playerY += ymove;
+    }
+    if(keyIsDown(87)){
+       playerY -= ymove;
+    }
+    if(keyIsDown(68)){
+       playerX += playerXmove;
+    }
+    if(keyIsDown(65)){
+       playerX -= playerXmove;
+    }
+       rect(playerX, playerY, 30, 30);
+
+
+
+   }
+/*
+
+
 let playerX = 300, playerY = 0, w = 0, h = 0;
 let bounceplayerY = false, bounceplayerX = false;
 let bounceShipY = false; bounceShipX = false;
@@ -74,9 +132,11 @@ function keyPressed(){
     }
 return true;
 }
-/*
-function mouseClicked(){
-    playerX = random(15,585);
-    playerY = random(15,585);
-}
+
+//function mouseClicked(){
+//    playerX = random(15,585);
+//    playerY = random(15,585);
+//}
+
+
 */
